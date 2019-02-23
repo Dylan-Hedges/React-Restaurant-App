1a388e3932e784dad2c7e1e19632a3687ad08bc1
DylanHedges <d.hedges1@hotmail.co.uk>
2019-02-23 09:49:07 -0500

    Converted css to scss, added google fonts, updated styling landing page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
-import Header from './Header.js';
+import './App.scss';
 import TopImg from './TopImg.js';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Header />
         <TopImg />
       </div>
     );
--- src/TopImg.js
+++ src/TopImg.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
+import Header from './Header.js';
 
 export default class TopImg extends Component {
   render() {
     return (
       <section className="TopImg">
-        <div className="title">
-          <h5>
-            Welcome
-          </h5>
-          <h1>
-            Prime Steak
-          </h1>
-        </div>
-        <div className="contact-info">
-          <div>Book a Table Directly</div>
-          <h2>(718) - 219 - 8652</h2>
-          <div className="hours">
-            Opening Hours
-            <strong>Mon - Fri:</strong> 9am - 9pm
-            <strong>Weekend:</strong> 9am - 11pm
+        <div className="container">
+          <Header />
+          <div className="title">
+            <h5>
+              Welcome
+            </h5>
+            <h1>
+              Prime Steak
+            </h1>
+          </div>
+          <div className="contact-info">
+            <div className="booking">Book a Table Directly</div>
+            <h2>(718) - 219 - 8652</h2>
+            <div className="hours">
+              Opening Hours
+              <strong> Mon - Fri: </strong> 9am - 9pm
+              <strong> Weekend: </strong> 9am - 11pm
+            </div>
           </div>
         </div>
       </section>
