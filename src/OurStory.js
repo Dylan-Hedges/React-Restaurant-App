8a9b5076e00731a866db0db515688f7e3461411f
DylanHedges <d.hedges1@hotmail.co.uk>
2019-02-23 17:19:51 -0500

    Added content to Our Story section

--- src/OurStory.js
+++ src/OurStory.js
@@ -8,7 +8,7 @@ export default class OurStory extends Component {
       <section id="OurStory">
         <div className="container">
           <div className="row">
-            <div clasName="col-md-6">
+            <div className="col-sm-6">
               <h5 className="comp-title">Our Story</h5>
               <h2>Cooking is the art of adjustment</h2>
               <p>Commodo esse veniam quo irure te ea te cohaerescant ad quorum cupidatat qui
@@ -16,10 +16,12 @@ export default class OurStory extends Component {
               iudicem, excepteur ita mandaremus, deserunt eruditionem e mentitum. An deserunt
               aut laborum do quis ad voluptate. Litteris hic sint deserunt in sint nostrud e
               probant, ea ab summis sunt sunt, laboris duis ubi constias voluptatibus.</p>
-              <div className="quote">The best steak in the town of Huntington - Thomas Eggsy</div>
+              <div className="quote">The best steak in the town of Huntington - <strong>Thomas Eggsy</strong></div>
               <a href="#" className="reserve-btn">Reserve</a>
             </div>
-            <div clasName="col-md-6">
+            <div className="col-sm-6">
+              <div className="video-image">
+              </div>
             </div>
           </div>
         </div>
