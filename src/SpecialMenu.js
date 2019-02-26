e3c013c0930b232cc8e3f8f9f9acfb212515f684
DylanHedges <d.hedges1@hotmail.co.uk>
2019-02-26 07:39:50 -0500

    Added content to Special Menu Component

--- src/SpecialMenu.js
+++ src/SpecialMenu.js
@@ -8,18 +8,36 @@ export default class SpecialMenu extends Component {
         <div className="container">
           <h5 className="comp-title">Special Menu</h5>
           <h2>Delicious Flavour of Autumn</h2>
-          <div className="row">
+          <div className="row boxes">
             <div className="col-md-4">
-              Box
+              <div className="box">
+                <div className="box-img">
+                  <div className="price-circle">$25</div>
+                </div>
+                <div className="title">Super BBQ Grill No Smoke</div>
+                <p className="details">Fried eggs, Steak, Baked potato or French Fries, side of vegetables</p>
+              </div>
             </div>
             <div className="col-md-4">
-              Box
+            <div className="box">
+              <div className="box-img">
+                <div className="price-circle">$25</div>
+              </div>
+              <div className="title">Super BBQ Grill No Smoke</div>
+              <p className="details">Fried eggs, Steak, Baked potato or French Fries, side of vegetables</p>
+            </div>
             </div>
             <div className="col-md-4">
-              Box
+            <div className="box">
+              <div className="box-img">
+                <div className="price-circle">$25</div>
+              </div>
+              <div className="title">Super BBQ Grill No Smoke</div>
+              <p className="details">Fried eggs, Steak, Baked potato or French Fries, side of vegetables</p>
+            </div>
             </div>
           </div>
-          <a href="#">View Full Menu</a>
+          <a href="#" className="link">View Full Menu</a>
         </div>
       </section>
     );
