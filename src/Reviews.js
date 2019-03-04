4bcb98dd997551da05ef2afd0c6b494ae1818214
DylanHedges <d.hedges1@hotmail.co.uk>
2019-03-04 07:33:56 -0500

    Added Reviews component conent

--- src/Reviews.js
+++ src/Reviews.js
@@ -7,14 +7,26 @@ export default class Reviews extends Component {
     return (
       <section id="Reviews">
         <div className="container">
-          <h5 className="comp-title">REVIEWS</h5>
-          <h2>The Food Network</h2>
-          <h4>"Best Restaurant in the L.A area!"</h4>
-          <p>Pariatur transferrem hic arbitror, o summis commodo pariatur, doctrina te quis 
-          singulis, aut expetendis ea mentitum. Minim ullamco philosophari, eu summis nam
-          tempor. De aut labore eiusmod.</p>
-          <div className="author"></div>
-          <div className="arrows"></div>
+          <div className="row">
+            <div className="col-md-8">
+              <div className="side-img">
+                <img src="https://img-aws.ehowcdn.com/600x600p/photos.demandstudios.com/getty/article/85/121/skd181312sdc.jpg" />
+              </div>
+            </div>
+            <div className="col-md-4">
+              <h5 className="comp-title">REVIEWS</h5>
+              <h2>The Food Network</h2>
+              <h4>"Best Restaurant in the L.A area!"</h4>
+              <p>Pariatur transferrem hic arbitror, o summis commodo pariatur, doctrina te quis
+              singulis, aut expetendis ea mentitum. Minim ullamco philosophari, eu summis nam
+              tempor. De aut labore eiusmod.</p>
+              <div className="author"><strong>Sarah Bastiachi</strong> - <em>winner of the chef masters</em></div>
+              <div className="arrows">
+                <i class="fas fa-arrow-left"></i>
+                <i class="fas fa-arrow-right ready"></i>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     );
