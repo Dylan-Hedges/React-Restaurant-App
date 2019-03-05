4858e19ad45cd1e7284dcc72a10fbf762ed0c417
DylanHedges <d.hedges1@hotmail.co.uk>
2019-03-05 07:44:56 -0500

    Added Contact Us content

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -11,15 +11,36 @@ export default class ContactUs extends Component {
           <div className="box">
             <div className="row">
               <div className="col-md-6">
-                <div className="city">
+                <div className="title">
                   Los Angeles, California
                 </div>
                 <h6 className="Address">
+                  535 main str<br/>
+                  Hollywood, CA 12402
                 </h6>
                 <p><strong>email:</strong><a href="mailto:info@primesteak.com">info@primesteak.com</a></p>
               </div>
               <div className="col-md-6">
-                Two
+                <h6>
+                  Phone:
+                </h6>
+                <div className="title">
+                  342-346-6789
+                </div>
+                <h6>
+                  Lunch Service
+                </h6>
+                <p>
+                  Friday, Saturday and Sunday<br/>
+                  from 12pm - 1:30pm
+                </p>
+                <h6>
+                  Dinner Service
+                </h6>
+                <p>
+                  Daily<br/>
+                  from 6pm - 11:30pm
+                </p>
               </div>
             </div>
           </div>
