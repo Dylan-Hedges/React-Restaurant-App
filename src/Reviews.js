9cfbdf911a1f5308ea765bb3d425421833f611f2
DylanHedges <d.hedges1@hotmail.co.uk>
2019-03-08 08:05:04 -0500

    Added reviews data, added display reviews data on screen functionality

--- src/Reviews.js
+++ src/Reviews.js
@@ -3,6 +3,23 @@ import './App.scss';
 import Header from './Header.js';
 
 export default class Reviews extends Component {
+
+  currentReview() {
+    return(
+      <div>
+        <h5 className="comp-title">REVIEWS</h5>
+        <h2>{this.props.data.reviews[this.props.data.reviewnumber.currentreview].company}</h2>
+        <h4>{this.props.data.reviews[this.props.data.reviewnumber.currentreview].highlight}</h4>
+        <p>{this.props.data.reviews[this.props.data.reviewnumber.currentreview].review}</p>
+        <div className="author"><strong>{this.props.data.reviews[this.props.data.reviewnumber.currentreview].author}</strong> - <em>{this.props.data.reviews[this.props.data.reviewnumber.currentreview].authorInfo}</em></div>
+        <div className="arrows">
+          <i class="fas fa-arrow-left"></i>
+          <i class="fas fa-arrow-right ready"></i>
+        </div>
+      </div>
+    );
+  }
+
   render() {
     return (
       <section id="Reviews">
@@ -14,17 +31,7 @@ export default class Reviews extends Component {
               </div>
             </div>
             <div className="col-md-4">
-              <h5 className="comp-title">REVIEWS</h5>
-              <h2>The Food Network</h2>
-              <h4>"Best Restaurant in the L.A area!"</h4>
-              <p>Pariatur transferrem hic arbitror, o summis commodo pariatur, doctrina te quis
-              singulis, aut expetendis ea mentitum. Minim ullamco philosophari, eu summis nam
-              tempor. De aut labore eiusmod.</p>
-              <div className="author"><strong>Sarah Bastiachi</strong> - <em>winner of the chef masters</em></div>
-              <div className="arrows">
-                <i class="fas fa-arrow-left"></i>
-                <i class="fas fa-arrow-right ready"></i>
-              </div>
+              {this.currentReview()}
             </div>
           </div>
         </div>
--- src/data.js
+++ src/data.js
@@ -23,10 +23,70 @@ const specialmenu =[
     price: 25
   }
 ]
+
+const reviews = [
+    {
+      company: 'The Food Network',
+      highlight: 'Best Restaurant in the L.A area!',
+      author: 'Sarah Bastiachi',
+      authorInfo: 'winner of the chef masters',
+      review: 'Pariatur transferrem hic arbitror, o summis commodo pariatur, doctrina te quis singulis, aut expetendis ea mentitum. Minim ullamco philosophari, eu summis namtempor. De aut labore eiusmod.'
+    },
+    {
+      company: 'HBO',
+      highlight: 'Best Restaurant in the L.A area!',
+      author: 'Sarah Bastiachi',
+      authorInfo: 'winner of the chef masters',
+      review: 'Pariatur transferrem hic arbitror, o summis commodo pariatur, doctrina te quis singulis, aut expetendis ea mentitum. Minim ullamco philosophari, eu summis namtempor. De aut labore eiusmod.'
+    },
+    {
+      company: 'CBC',
+      highlight: 'Best Restaurant in the L.A area!',
+      author: 'Sarah Bastiachi',
+      authorInfo: 'winner of the chef masters',
+      review: 'Pariatur transferrem hic arbitror, o summis commodo pariatur, doctrina te quis singulis, aut expetendis ea mentitum. Minim ullamco philosophari, eu summis namtempor. De aut labore eiusmod.'
+    },
+    {
+      company: 'MSNBC',
+      highlight: 'Best Restaurant in the L.A area!',
+      author: 'Sarah Bastiachi',
+      authorInfo: 'winner of the chef masters',
+      review: 'Pariatur transferrem hic arbitror, o summis commodo pariatur, doctrina te quis singulis, aut expetendis ea mentitum. Minim ullamco philosophari, eu summis namtempor. De aut labore eiusmod.'
+    },
+    {
+      company: 'New York Times',
+      highlight: 'Best Restaurant in the L.A area!',
+      author: 'Sarah Bastiachi',
+      authorInfo: 'winner of the chef masters',
+      review: 'Pariatur transferrem hic arbitror, o summis commodo pariatur, doctrina te quis singulis, aut expetendis ea mentitum. Minim ullamco philosophari, eu summis namtempor. De aut labore eiusmod.'
+    }
+  ]
+
+const reviewnumber = {
+  currentreview: 0
+}
+
+const randomquote = [
+  {
+    authro: 'John Harrod',
+    quote: ' "Simple ingredients prepared in a simple way - thats the best way to take your everyday cooking to a higher level." '
+  },
+  {
+    authro: 'Ben Smith',
+    quote: ' "Simple ingredients prepared in a simple way - thats the best way to take your everyday cooking to a higher level." '
+  },
+  {
+    authro: 'Jenna Tyson',
+    quote: ' "Simple ingredients prepared in a simple way - thats the best way to take your everyday cooking to a higher level." '
+  },
+]
+
 //Must come last
 const data = {
   topimg,
   contactus,
-  specialmenu
+  specialmenu,
+  reviews,
+  reviewnumber
 }
 export default data;
