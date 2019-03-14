cee4149efabb53b69005a9ccb5e95afb725d343b
DylanHedges <d.hedges1@hotmail.co.uk>
2019-03-14 08:17:05 -0400

    Updated OurStory content and styling

--- src/OurStory.js
+++ src/OurStory.js
@@ -9,13 +9,13 @@ export default class OurStory extends Component {
           <div className="row">
             <div className="col-sm-6">
               <h5 className="comp-title">Our Story</h5>
-              <h2>Cooking is the art of adjustment</h2>
+              <h2>Slow smoked ribs, chicken and brisket made in house.</h2>
               <p>Commodo esse veniam quo irure te ea te cohaerescant ad quorum cupidatat qui
               pariatur et a sint fore aut mentitum. In singulis domesticarum hic velit se
               iudicem, excepteur ita mandaremus, deserunt eruditionem e mentitum. An deserunt
               aut laborum do quis ad voluptate. Litteris hic sint deserunt in sint nostrud e
               probant, ea ab summis sunt sunt, laboris duis ubi constias voluptatibus.</p>
-              <div className="quote">The best steak in the town of Huntington - <strong>Thomas Eggsy</strong></div>
+              <div className="quote">The best ribs in the city! - <strong>Thomas Backstone</strong></div>
               <a href="#" className="reserve-btn">Reserve</a>
             </div>
             <div className="col-sm-6">
