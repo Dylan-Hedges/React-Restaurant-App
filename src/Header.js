b3562b05d82dcb4de6572be40e60aacda27d822e
DylanHedges <d.hedges1@hotmail.co.uk>
2019-09-01 11:00:23 -0400

    Added navbar (bootstrap) - added JS CDN Bootstrap links for collapse

--- src/Header.js
+++ src/Header.js
@@ -7,26 +7,23 @@ class Header extends Component {
   render() {
     return (
       <header id="Header">
-        <div className="logo">
-          <a href="#"><img src={logo}/></a>
+        <div>
+          <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
+            <Link to="/"><img src={logo} className="logo"/></Link>
+            <button className="navbar-toggler" data-toggle="collapse" data-target="#navcollapseid">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navcollapseid">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item"><Link className="headerMenu" activeClass="active" to="OurStory" spy={true} smooth={true} offset={0} duration= {1000}>Our Story</Link></li>
+                <li className="nav-item"><Link className="headerMenu" activeClass="active" to="SpecialMenu" spy={true} smooth={true} offset={0} duration= {1500}>Special Menu</Link></li>
+                <li className="nav-item"><Link className="headerMenu" activeClass="active" to="Reviews" spy={true} smooth={true} offset={0} duration= {1250}>Reviews</Link></li>
+                <li className="nav-item"><Link className="headerMenu" activeClass="active" to="ContactUs" spy={true} smooth={true} offset={0} duration= {1750}>Reservations</Link></li>
+                <li className="nav-item"><Link className="headerMenu" activeClass="active" to="ContactUs" spy={true} smooth={true} offset={0} duration= {2000}>Contact Us</Link></li>
+              </ul>
+            </div>
+          </nav>
         </div>
-        <nav>
-          <Link className="headerMenu" activeClass="active" to="OurStory" spy={true} smooth={true} offset={0} duration= {1000}>
-            Our Story
-          </Link>
-          <Link className="headerMenu" activeClass="active" to="SpecialMenu" spy={true} smooth={true} offset={0} duration= {1500}>
-            Special Menu
-          </Link>
-          <Link className="headerMenu" activeClass="active" to="Reviews" spy={true} smooth={true} offset={0} duration= {1250}>
-            Reviews
-          </Link>
-          <Link className="headerMenu" activeClass="active" to="ContactUs" spy={true} smooth={true} offset={0} duration= {1750}>
-            Reservations
-          </Link>
-          <Link className="headerMenu" activeClass="active" to="ContactUs" spy={true} smooth={true} offset={0} duration= {2000}>
-            Contact Us
-          </Link>
-        </nav>
       </header>
     );
   }
